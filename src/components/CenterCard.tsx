13d83a6f8691eb42494fe7b841a2247d090aab5f
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2021-05-02 14:23:31 +0530

    added link to google maps for centers

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -31,25 +31,6 @@ const CenterCard: React.FC<{ center: Center }> = ({ center }) => {
     >
       <div className="flex justify-between mb-4">
         <h2 className="text-lg font-semibold">{center.name}</h2>
-        <button
-          onClick={handleCopyButtonClick}
-          className="p-2 rounded-md hover:bg-gray-200"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-            />
-          </svg>
-        </button>
       </div>
       <div className="text-xs">
         <h4 className="font-medium mb-2 uppercase text-gray-500 tracking-wide">
@@ -82,6 +63,54 @@ const CenterCard: React.FC<{ center: Center }> = ({ center }) => {
           {!validSessions.length && (
             <span>There are available session here</span>
           )}
+
+          <div className="flex">
+            <a
+              target="_blank"
+              href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
+                [
+                  center.name,
+                  center.district_name,
+                  center.pincode,
+                  center.sessions,
+                ].join(", ")
+              )}`}
+            >
+              <button className="p-2 rounded-md hover:bg-gray-200 mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </a>
+            <button
+              onClick={handleCopyButtonClick}
+              className="p-2 rounded-md hover:bg-gray-200"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
