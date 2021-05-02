3a613c9075e785c6fdf9a4b165ea2073a791c5b4
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2021-05-02 12:12:46 +0530

    fix: added sorting on availabilit

--- src/constants/seo-constants.ts
+++ src/constants/seo-constants.ts
@@ -3,17 +3,17 @@ export const getURL = (): string => {
     process?.env?.URL && process.env.URL !== ''
       ? process.env.URL
       : process?.env?.VERCEL_URL && process.env.VERCEL_URL !== ''
-      ? process.env.VERCEL_URL
-      : 'http://localhost:3000'
+        ? process.env.VERCEL_URL
+        : 'http://localhost:3000'
   return url.includes('http') ? url : `https://${url}`
 }
 
 const DEFAULT_TITLE = ''
-const DEFAULT_TITLE_TEMPLATE = 'Nextjs Starter | %s'
+const DEFAULT_TITLE_TEMPLATE = 'Cowin | %s'
 const DEFAULT_DESCRIPTION =
   'A Next.js starter kit template with React 17 + Typescript + Tailwind CSS 2 + React Query 3 + GitHub Auth + Passwordless Auth + Fauna DB.'
 const DEFAULT_CANONICAL = getURL()
-const SITE_NAME = 'Nextjs Starter'
+const SITE_NAME = 'Cowin'
 const DEFAULT_OG_IMAGE = `${DEFAULT_CANONICAL}/preview.png`
 const TWITTER_HANDLE = '@pbteja1998'
 const TWITTER_CARD_TYPE = 'summary_large_image'
