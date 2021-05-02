9611e34c5229a1b1a9b877f8dce38dd70e6e4540
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2021-05-02 11:53:58 +0530

    added button to checkout cowin website

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -1,12 +1,44 @@
+import { copyTextToClipboard } from "@/utils/copyToClipboard";
+import cx from "classNames";
 import React from "react";
 import { Center } from "src/services/getSessions";
 
-const dateFormat = (date: string) => {};
+const dateFormat = (date: string) => {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+  }).format(new Date(date.split("-").reverse().join("-")));
+};
 
 const CenterCard: React.FC<{ center: Center }> = ({ center }) => {
+  const handleCopyButtonClick = () => {
+    copyTextToClipboard(center.name);
+    window.open("https://selfregistration.cowin.gov.in/appointment", "_blank");
+  };
   return (
     <div className="p-4 rounded-md  broder border border-gray-300 flex flex-col justify-between">
-      <h2 className="text-lg font-semibold mb-4">{center.name}</h2>
+      <div className="flex justify-between">
+        <h2 className="text-lg font-semibold mb-4">{center.name}</h2>
+        <button
+          onClick={handleCopyButtonClick}
+          className="p-4 rounded-md hover:bg-gray-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </button>
+      </div>
       <div className="text-xs">
         <h4 className="font-medium mb-2 uppercase text-gray-500 tracking-wide">
           Sessions
@@ -14,10 +46,17 @@ const CenterCard: React.FC<{ center: Center }> = ({ center }) => {
         <div>
           {center.sessions.map((s) => (
             <span
-              className="mr-2 mb-2 inline-block py-1 px-2 border border-gray-300 rounded-md"
+              className={cx(
+                "mr-2 mb-2 inline-block py-1 px-2 rounded-md",
+                !s.available_capacity && "border border-gray-300",
+                s.available_capacity > 10 && "text-white bg-indigo-700",
+                !!s.available_capacity &&
+                  s.available_capacity <= 10 &&
+                  "bg-red-400 text-white"
+              )}
               key={s.session_id}
             >
-              {s.date}
+              {dateFormat(s.date)}
             </span>
           ))}
         </div>
